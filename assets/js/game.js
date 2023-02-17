8d6c6ec27f51e39866610c1a2bdf9d051a16d118
Nicolas Tosoroni <nicolas.tosoroni@externos-ar.cencosud.com>
2023-02-17 18:10:49 -0300

    feat: se agrega funcionalidad para verificar el valor de la carta

--- assets/js/game.js
+++ assets/js/game.js
@@ -30,11 +30,35 @@ const createDesk = () => {
     }
   }
   console.log(deck);
-  deck = fyShuffle(deck)
+  deck = fyShuffle(deck);
   console.log(deck);
-  return deck
+  return deck;
 };
 
 createDesk();
 
 //lib undescore para mezclar
+
+const requestCard = () => {
+  // tengo que sacar la carta del deck
+  if (deck.length === 0) {
+    throw 'No hay cartas en el deck';
+  }
+  const card = deck.shift();
+  return card;
+};
+
+const cardValue = (card) => {
+  // let value
+  // if (card.length === 2){
+  //   value = card[0]
+  // }else{
+  //   value = card[0] + card[1]
+  // }
+  const value = card.substring(0, card.length - 1);
+  return isNaN(value)
+    ? (points = value === 'A' ? 11 : 10)
+    : (points = parseInt(value));
+};
+
+cardValue('2D');
